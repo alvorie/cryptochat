988b20c7b0245dad2d90ed7a7c16c6177b1a34d1
alvorie <123foggy123@gmail.com>
2025-03-24 14:24:23 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import CryptoList from "./components/CryptoList";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQueryClient,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Chat from "./components/Chat";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 function App() {
-  const [count, setCount] = useState(0);
   const queryClient = new QueryClient();
 
   return (
